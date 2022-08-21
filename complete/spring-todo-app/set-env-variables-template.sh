d70ebad1816d4bd7c2d6fc26d4e3f31c7d8c8a37
Salma Elbakry <79708975+salmamoh01@users.noreply.github.com>
2022-08-21 08:11:58 +0200

    Add files via upload

---
+++ complete/spring-todo-app/set-env-variables-template.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+export COSMOS_URI=<put-your-COSMOS-DB-documentEndpoint-URI-here>
+export COSMOS_KEY=<put-your-COSMOS-DB-primaryMasterKey-here>
+export COSMOS_DATABASE=<put-your-COSMOS-DATABASE-name-here>
